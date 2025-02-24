ee097145c1bf222bd685693f9e51c569c2f56713
AdrianRuchala <adrianruchala03@gmail.com>
2025-02-24 10:42:04 +0100

    Initialized Spring Boot project

---
+++ src/main/kotlin/com/example/SwiftCodeParser/SwiftCodeParserApplication.kt
@@ -0,0 +1,11 @@
+package com.example.SwiftCodeParser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwiftCodeParserApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwiftCodeParserApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/SwiftCodeParser/SwiftCodeParserApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.SwiftCodeParser
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwiftCodeParserApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
